3f8d9292e01e1529f5be94334f0975e762145898
jesseYeh0319 <yehjesse96@gmail.com>
2025-05-10 17:56:50 +0800

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,10 +1,13 @@
+import groovy.json.JsonOutput
+
 def call(String message, String emoji = ":rocket:") {
   withCredentials([string(credentialsId: 'slack-webhook', variable: 'SLACK_URL')]) {
     def payload = [
       text: "${emoji} Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} ${message}\n👉 ${env.BUILD_URL}"
     ]
-    writeJSON file: 'slack-payload.json', json: payload
+
+    writeFile file: 'slack-payload.json', text: JsonOutput.toJson(payload)
+
     sh 'curl -X POST -H "Content-type: application/json" --data @slack-payload.json "$SLACK_URL"'
   }
 }
-
